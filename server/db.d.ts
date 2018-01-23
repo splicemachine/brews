5490727d80ed6f9c023647b7d3ffa3cb34ccf1cc
Nikhil Nygaard <nikhil.nygaard@gmail.com>
2018-01-23 14:12:35 -0800

    forgot to add

---
+++ server/db.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Really trivial types for PureCSS
+ * I might want to commit these types to @types
+ */
+
+// declare module "db" {
+//     function select(
+//         db: any,
+//         name: string
+//     ): Array<any>;
+// }
