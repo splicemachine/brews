930183e94f8a14c599f2afae3ab91be5c1767f9e
Nikhil Nygaard <nikhil.nygaard@gmail.com>
2018-03-02 13:26:40 -0800

    validating forms and splitting button logic for gene's demo

--- client/components/ATP.jsx
+++ client/components/ATP.jsx
@@ -13,6 +13,7 @@ export default class ATP extends Component {
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.disable = this.disable.bind(this);
     }
 
     handleChange(key, event) {
@@ -38,55 +39,100 @@ export default class ATP extends Component {
         // });
     }
 
-    handleSubmit(event) {
-        console.log(this.state);
+    handleSubmit(form, event) {
+        switch(form){
+            case "addLine":
+                console.log(form, event);
+                break;
+            case "runATP":
+                console.log(form, event);
+                break;
+            case "clearLines":
+                console.log(form, event);
+                break;
+            default:
+                console.log("I don't know what that one is.");
+                break;
+        }
         event.preventDefault();
+    }
+
+    disable(form){
+        switch(form){
+            case "addLine":
+                return this.state.quantity === "" || this.state.itemNumber === "";
+            case "runATP":
+                return this.state.targetDate === "";
+            case "clearLines":
+                return false;
+            default:
+                return true;
+        }
     }
 
     render() {
         return (
-
             <div className="content">
                 <h2 className="content-subhead">ATP</h2>
-
-                <form className="pure-form pure-form-aligned" onSubmit={this.handleSubmit}>
-                    <fieldset>
-                        <div className="pure-control-group">
-                            <label htmlFor="quantity">Quantity</label>
-                            <input
-                                required
-                                id="quantity"
-                                type="number"
-                                placeholder="Quantity"
-                                value={this.state.quantity}
-                                onChange={this.handleChange.bind(this, "quantity")}/>
-                        </div>
-                        <div className="pure-control-group">
-                            <label htmlFor="itemNumber">Item Number</label>
-                            <input
-                                required
-                                id="itemNumber"
-                                type="number"
-                                placeholder="Item Number"
-                                value={this.state.itemNumber}
-                                onChange={this.handleChange.bind(this, "itemNumber")}/>
-                        </div>
-                        <div className="pure-control-group">
-                            <label htmlFor="targetDate">Target Date</label>
-                            <input
-                                required
-                                id="targetDate"
-                                type="date"
-                                placeholder="Target Date"
-                                value={this.state.targetDate}
-                                onChange={this.handleChange.bind(this, "targetDate")}/>
-                        </div>
-
-                        <div className="pure-controls">
-                            <button type="submit" className="pure-button pure-button-primary" disabled={!this.state.formComplete}>Submit</button>
-                        </div>
-                    </fieldset>
-                </form>
+                <div className="pure-g">
+                    <div className={"pure-u-1 pure-u-md-1-2 pure-u-lg-1-2"}>
+                        <form className="pure-form pure-form-aligned" onSubmit={this.handleSubmit.bind(this, "addLine")}>
+                            <fieldset>
+                                <div className="pure-control-group">
+                                    <label htmlFor="quantity">Quantity</label>
+                                    <input
+                                        required
+                                        id="quantity"
+                                        type="number"
+                                        placeholder="Quantity"
+                                        value={this.state.quantity}
+                                        onChange={this.handleChange.bind(this, "quantity")}/>
+                                </div>
+                                <div className="pure-control-group">
+                                    <label htmlFor="itemNumber">Item Number</label>
+                                    <input
+                                        required
+                                        id="itemNumber"
+                                        type="number"
+                                        placeholder="Item Number"
+                                        value={this.state.itemNumber}
+                                        onChange={this.handleChange.bind(this, "itemNumber")}/>
+                                </div>
+                                <div className="pure-controls">
+                                    <button type="submit" className="pure-button pure-button-primary" disabled={this.disable("addLine")}>Add Line</button>
+                                </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                    <div className={"pure-u-1 pure-u-md-1-2 pure-u-lg-1-2"}>
+                        <form className="pure-form pure-form-aligned" onSubmit={this.handleSubmit.bind(this, "runATP")}>
+                            <fieldset>
+                                <div className="pure-control-group">
+                                    <label htmlFor="targetDate">Target Date</label>
+                                    <input
+                                        required
+                                        id="targetDate"
+                                        type="date"
+                                        placeholder="Target Date"
+                                        value={this.state.targetDate}
+                                        onChange={this.handleChange.bind(this, "targetDate")}/>
+                                </div>
+                                <div className="pure-controls">
+                                    <button type="submit" className="pure-button pure-button-primary" disabled={this.disable("runATP")}>Run ATP</button>
+                                </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                    <div className={"pure-u-1 pure-u-md-1-1 pure-u-lg-1-1"}>
+                        <form className="pure-form pure-form-aligned" onSubmit={this.handleSubmit.bind(this, "clearLines")}>
+                            <fieldset>
+                                <div className="pure-controls">
+                                    <button type="submit" className="pure-button pure-button-primary" disabled={this.disable("clearLines")}>Clear Lines</button>
+                                </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
