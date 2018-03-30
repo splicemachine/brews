ef580286def855a96e2d0b3abf0e3d03b608f64c
Nikhil Nygaard <nikhil.nygaard@gmail.com>
2018-03-29 21:53:41 -0700

    sample backend

---
+++ server/routers/modeling/handlers.ts
@@ -0,0 +1,22 @@
+import express = require("express");
+import {sample_datasets, output_sample, job_status, table_data} from "./test_data";
+
+export function models(request: express.Request, response: express.Response){
+    response.send(table_data);
+}
+
+export function datasets(request: express.Request, response: express.Response){
+    response.send(sample_datasets);
+}
+
+export function action(request: express.Request, response: express.Response){
+    response.send("OK");
+}
+
+export function jobs(request: express.Request, response: express.Response){
+    response.send(job_status);
+}
+
+export function output(request: express.Request, response: express.Response){
+    response.send(output_sample);
+}
